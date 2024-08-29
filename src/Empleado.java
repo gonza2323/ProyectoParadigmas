4fd1f674514c7c3adfaf50b4437cadaf7ad4965d
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2024-08-28 21:21:07 -0300

    Project setup

---
+++ src/Empleado.java
@@ -0,0 +1,18 @@
+
+
+public class Empleado {
+    private String nombre;
+    private int dni;
+}
+
+class Cajero extends Empleado {
+
+}
+
+class Gerente extends Empleado {
+
+}
+
+class AgenteEspecial extends Empleado {
+
+}
