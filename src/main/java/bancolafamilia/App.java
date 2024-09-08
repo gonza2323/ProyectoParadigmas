4531ee12c910285f6bb8bcba47ae6f2d7dc9ca60
Perla <valerioperla98@gmail.com>
2024-09-08 14:18:42 -0300

    update

--- src/main/java/bancolafamilia/App.java
+++ src/main/java/bancolafamilia/App.java
@@ -1,5 +1,6 @@
 package bancolafamilia;
 
+import bancolafamilia.banco.AgenteEspecial;
 import bancolafamilia.banco.Banco;
 import bancolafamilia.banco.Cliente;
 import bancolafamilia.banco.Gerente;
@@ -15,15 +16,19 @@ public class App {
         Cliente cliente1 = new Cliente("Martin", 1234, "martin", "1234");
         Cliente cliente2 = new Cliente("Jorge", 1235, "jorge", "1234");
         Cliente cliente3 = new Cliente("Pedro", 1236, "pedro", "1234");
+        Cliente cliente4 = new Cliente("Carlos", 1238, "carlos", "1234");
 
         banco.addUser(cliente1);
         banco.addUser(cliente2);
         banco.addUser(cliente3);
+        banco.addUser(cliente4);
         //banco.addUser(gerente);
 
         cliente1.setAlias("hola.como.estas");
         cliente2.setAlias("muy.bien.tu");
         cliente3.setAlias("muchas.gracias.chau");
+        cliente4.setAlias("mapa.fiar.oro"); //alias asistente ejecutivo
+
 
         banco.depositFunds(cliente1, 10000);
         banco.depositFunds(cliente2, 2000);
@@ -33,6 +38,7 @@ public class App {
         banco.addUser(new Gerente("admin", 1237, "admin", "hunter2"));
         //banco.addUser(gerente);
         //banco.addEmpleado(gerente);
+        banco.addUser(new AgenteEspecial("carlos", 1238, "especial", "hunter3"));
 
         Interfaz interfaz = new Interfaz(banco);
         interfaz.start();
